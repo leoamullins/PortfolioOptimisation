e4b261317d86c400fa487d115eb08b035d5b2be5
leoamullins <leomullins18@gmail.com>
2025-09-25 02:00:30 +0100

    added backtesting to test_ef

--- test_ef.py
+++ test_ef.py
@@ -1,10 +1,17 @@
 import yfinance as yf
 from EfficientFrontier import ef
+from BacktestFramework import backtester, max_sharpe_wrapper
 
 tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "NVDA", "JPM", "XOM", "PG", "V"]
-prices = yf.download(tickers, start = '2015-01-01', auto_adjust=False)['Adj Close']
+prices = yf.download(tickers, start = '2012-01-01', auto_adjust=False)['Adj Close']
 
 ef1 = ef(prices) # Initialising as Efficient Frontier
 
 val, weights = ef1.max_sharpe(prices)
-ef1.plotting(prices = prices , maxsharpe=True)
+
+backtester1 = backtester(prices, max_sharpe_wrapper, rebalance_freq=21)
+backtester1.prepare_data()
+history = backtester1.run()
+
+print(history)
+print(weights)
